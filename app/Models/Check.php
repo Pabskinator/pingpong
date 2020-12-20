0c29fccc0618bb192ae4500344a2d4a19f97808c
Adrian Pabe <adrianpabs14@gmail.com>
2020-12-20 19:25:44 +0800

    Install ide helper

--- app/Models/Check.php
+++ app/Models/Check.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Check
+ *
+ * @property int $id
+ * @property int $response_status
+ * @property string $response_content
+ * @property int $elapsed_time
+ * @property int $site_id
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|Check newModelQuery()
+ * @method static Builder|Check newQuery()
+ * @method static Builder|Check query()
+ * @method static Builder|Check whereCreatedAt($value)
+ * @method static Builder|Check whereElapsedTime($value)
+ * @method static Builder|Check whereId($value)
+ * @method static Builder|Check whereResponseContent($value)
+ * @method static Builder|Check whereResponseStatus($value)
+ * @method static Builder|Check whereSiteId($value)
+ * @method static Builder|Check whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class Check extends Model
 {
     use HasFactory;
--- app/Models/Site.php
+++ app/Models/Site.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Site
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $url
+ * @property int|null $is_online
+ * @property int $user_id
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read User $user
+ * @method static Builder|Site newModelQuery()
+ * @method static Builder|Site newQuery()
+ * @method static Builder|Site query()
+ * @method static Builder|Site whereCreatedAt($value)
+ * @method static Builder|Site whereId($value)
+ * @method static Builder|Site whereIsOnline($value)
+ * @method static Builder|Site whereName($value)
+ * @method static Builder|Site whereUpdatedAt($value)
+ * @method static Builder|Site whereUrl($value)
+ * @method static Builder|Site whereUserId($value)
+ * @mixin Eloquent
+ */
 class Site extends Model
 {
     use HasFactory;
